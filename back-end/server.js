ddb94ec1250dd4d8c7c67d5298636e2fa96f6098
Jeswin Johnson <jeswinjohnson@Jeswins-MacBook-Air.local>
2026-03-09 19:47:35 +0200

    fix nodemailer smtp port

--- back-end/server.js
+++ back-end/server.js
@@ -15,9 +15,10 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.error(err));
 
-// Setup Nodemailer
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
