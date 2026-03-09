52911f788ca3bb70b2682bd2949b522aba9d10dc
Jeswin Johnson <jeswinjohnson@Jeswins-MacBook-Air.local>
2026-03-09 19:03:24 +0200

    ready for railway deploy

--- back-end/server.js
+++ back-end/server.js
@@ -191,4 +191,5 @@ app.delete("/bookings/:id", async (req, res) => {
 });
 
 // Start server
-app.listen(5000, () => console.log("Server running on port 5000")); 
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
